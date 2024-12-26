abd5aae279ba4498f0134be16e7c6ba6f90e9568
splamei <tylerwooler@gmail.com>
2024-12-26 13:58:12 +0000

    Made the minimise and maximise box on the about box disabled

--- Rhythm Plus - Splamei Client/AboutNew.Designer.cs
+++ Rhythm Plus - Splamei Client/AboutNew.Designer.cs
@@ -135,6 +135,8 @@
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "AboutNew";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
