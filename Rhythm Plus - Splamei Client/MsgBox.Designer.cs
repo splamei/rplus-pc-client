32600f2b45caae57e894b55150dcb0f3dc548b58
splamei <tylerwooler@gmail.com>
2025-03-15 14:19:49 +0000

    Adjusted the message box

--- Rhythm Plus - Splamei Client/MsgBox.Designer.cs
+++ Rhythm Plus - Splamei Client/MsgBox.Designer.cs
@@ -39,20 +39,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(119, 57);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 63);
+            this.label2.Size = new System.Drawing.Size(81, 51);
             this.label2.TabIndex = 5;
             this.label2.Text = "Box Text\r\n\r\nHello World!";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 45);
+            this.label1.Size = new System.Drawing.Size(88, 30);
             this.label1.TabIndex = 4;
             this.label1.Text = "Box title";
             // 
@@ -60,10 +60,10 @@
             // 
             this.pictureBox1.BackgroundImage = global::Rhythm_Plus___Splamei_Client.Properties.Resources.Info;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(20, 57);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(92, 88);
+            this.pictureBox1.Size = new System.Drawing.Size(64, 65);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -75,6 +75,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -84,6 +85,7 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // MsgBox
             // 
@@ -103,6 +105,7 @@
             this.Name = "MsgBox";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Message Box";
             this.Load += new System.EventHandler(this.MsgBox_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- Rhythm Plus - Splamei Client/MsgBox.cs
+++ Rhythm Plus - Splamei Client/MsgBox.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Rhythm_Plus___Splamei_Client
 {
     public partial class MsgBox : Form
     {
+        Action<int> actionToRun;
+
         public MsgBox()
         {
             InitializeComponent();
@@ -19,17 +22,47 @@ namespace Rhythm_Plus___Splamei_Client
 
         private void MsgBox_Load(object sender, EventArgs e)
         {
-
+            SystemSounds.Asterisk.Play();
         }
 
         public void setData(Action<int> action, string title, string text, string formTitle, string button1Text, string button2Text)
         {
             this.Text = formTitle;
-            label1.Text = text;
+            label1.Text = title;
+            label2.Text = text;
             button1.Text = button1Text;
             button2.Text = button2Text;
-            button1.Click += (sender, e) => { action(1); this.Close(); };
-            button2.Click += (sender, e) => { action(2); this.Close(); };
+            actionToRun = action;
+
+            if (button1Text == "")
+            {
+                button1.Visible = false;
+            }
+
+            if (button2Text == "")
+            {
+                button2.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (actionToRun != null)
+            {
+                actionToRun(1);
+            }
+
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (actionToRun != null)
+            {
+                actionToRun(2);
+            }
+
+            this.Close();
         }
     }
 }
