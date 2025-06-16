401c7b27b57a6ec563f186b836ce5cc2885a421c
splamei <instantveemo@gmail.com>
2025-06-16 20:34:19 +0100

    Fixed settings UI

--- Rhythm Plus - Splamei Client/Settings.Designer.cs
+++ Rhythm Plus - Splamei Client/Settings.Designer.cs
@@ -63,7 +63,7 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(12, 340);
+            this.checkBox1.Location = new System.Drawing.Point(12, 327);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(222, 24);
             this.checkBox1.TabIndex = 1;
@@ -76,7 +76,7 @@
             this.checkBox2.AutoSize = true;
             this.checkBox2.Checked = true;
             this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox2.Location = new System.Drawing.Point(12, 370);
+            this.checkBox2.Location = new System.Drawing.Point(12, 357);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(291, 24);
             this.checkBox2.TabIndex = 2;
@@ -87,7 +87,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(5, 246);
+            this.label2.Location = new System.Drawing.Point(5, 233);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(218, 30);
             this.label2.TabIndex = 3;
@@ -96,7 +96,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 285);
+            this.label3.Location = new System.Drawing.Point(7, 272);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(289, 40);
             this.label3.TabIndex = 4;
@@ -105,7 +105,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 595);
+            this.label4.Location = new System.Drawing.Point(8, 576);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(293, 20);
             this.label4.TabIndex = 5;
@@ -115,7 +115,7 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(12, 400);
+            this.checkBox3.Location = new System.Drawing.Point(12, 387);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(286, 44);
             this.checkBox3.TabIndex = 6;
@@ -125,7 +125,7 @@
             // trackBar1
             // 
             this.trackBar1.LargeChange = 1;
-            this.trackBar1.Location = new System.Drawing.Point(7, 486);
+            this.trackBar1.Location = new System.Drawing.Point(7, 473);
             this.trackBar1.Minimum = 1;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(254, 45);
@@ -136,7 +136,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 462);
+            this.label5.Location = new System.Drawing.Point(8, 449);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(199, 20);
             this.label5.TabIndex = 8;
@@ -146,7 +146,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(8, 525);
+            this.label6.Location = new System.Drawing.Point(8, 512);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(303, 39);
             this.label6.TabIndex = 9;
@@ -226,7 +226,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(320, 618);
+            this.ClientSize = new System.Drawing.Size(320, 605);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.checkBox6);
