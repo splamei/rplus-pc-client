e6c955a6eeabee163d3d7f38f5e878bd94209afb
splamei <instantveemo@gmail.com>
2025-06-19 17:22:50 +0100

    Updated Licences.cs

--- Rhythm Plus - Splamei Client/Licences.cs
+++ Rhythm Plus - Splamei Client/Licences.cs
@@ -20,22 +20,34 @@ namespace Rhythm_Plus___Splamei_Client
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Process.Start("https://github.com/Lachee/discord-rpc-csharp");
+            using (var process = Process.Start("https://github.com/Lachee/discord-rpc-csharp"))
+            {
+
+            }
         }
 
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Process.Start("https://www.nuget.org/packages/Microsoft.Web.WebView2/1.0.2957.106");
+            using (var process = Process.Start("https://www.nuget.org/packages/Microsoft.Web.WebView2/1.0.2957.106"))
+            {
+
+            }
         }
 
         private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Process.Start("https://www.nuget.org/packages/Newtonsoft.Json/13.0.1");
+            using (var process = Process.Start("https://www.nuget.org/packages/Newtonsoft.Json/13.0.1"))
+            {
+
+            }
         }
 
         private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Process.Start("https://www.nuget.org/packages/ILMerge/3.0.29");
+            using (var process = Process.Start("https://www.nuget.org/packages/ILMerge/3.0.29"))
+            {
+
+            }
         }
     }
 }
