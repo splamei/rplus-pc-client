e49fa301e898513fa87d7091724147b704e75fd9
splamei <instantveemo@gmail.com>
2025-06-19 16:52:28 +0100

    Updated AssemblyInfo.cs

--- Rhythm Plus - Splamei Client/Properties/AssemblyInfo.cs
+++ Rhythm Plus - Splamei Client/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rhythm Plus - Splamei Client")]
-[assembly: AssemblyDescription("Play the Rythm Plus Web Game in a custom client for Windows")]
+[assembly: AssemblyDescription("A custom client by Splamei for playing the Rhythm Plus web game")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Splamei")]
 [assembly: AssemblyProduct("Rhythm Plus - Splamei Client")]
-[assembly: AssemblyCopyright("Copyright © Splamei 2024-2025 (MIT Licence)")]
+[assembly: AssemblyCopyright("Copyright © Splamei 2024 (MIT Licence)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
