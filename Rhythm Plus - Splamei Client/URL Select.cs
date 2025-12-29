5f2cc923d8b33d54a03aa4faac286850ff705946
splamei <129879626+splamei@users.noreply.github.com>
2025-12-29 15:29:52 +0000

    Added a http warning

--- Rhythm Plus - Splamei Client/URL Select.cs
+++ Rhythm Plus - Splamei Client/URL Select.cs
@@ -19,7 +19,7 @@ namespace Rhythm_Plus___Splamei_Client
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (!textBox1.Text.StartsWith("https://"))
+            if (!textBox1.Text.StartsWith("https://") && !textBox1.Text.StartsWith("http://"))
             {
                 textBox1.Text = "https://" + textBox1.Text;
             }
@@ -31,13 +31,20 @@ namespace Rhythm_Plus___Splamei_Client
             }
             else
             {
-                textBox1.Text = "https://rhythm-plus.com";
-
                 using (MsgBox msgBox = new MsgBox())
                 {
-                    msgBox.setData(null, "Unable to navigate", "We can't go to that URL since it's not a Rhythm Plus URL\n\nPlease make sure the URL you're trying to go to starts with\nhttps://rhythm-plus.com then try again", "Unable to navigate - Splamei Client", "OK", "");
+                    if (textBox1.Text.StartsWith("http:"))
+                    {
+                        msgBox.setData(null, "Unable to navigate", "We can't go to that URL since it's a http URL not a https URL.\n\nPlease replace http:// with https:// so you can keep\nyourself safe while playing!", "Unable to navigate - Splamei Client", "OK", "");
+                    }
+                    else
+                    {
+                        msgBox.setData(null, "Unable to navigate", "We can't go to that URL since it's not a Rhythm Plus URL\n\nPlease make sure the URL you're trying to go to starts with\nhttps://rhythm-plus.com or https://v2.rhythm-plus.com\nthen try again", "Unable to navigate - Splamei Client", "OK", "");
+                    }
                     msgBox.ShowDialog();
                 }
+
+                textBox1.Text = "https://rhythm-plus.com";
             }
         }
 
